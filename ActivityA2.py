ca8f74b6e8b56498722a77880ecf017ff4d38fd0
vikaswilson1980-creator <vikaswilson1980@gmail.com>
2025-09-24 20:36:58 +0530

    Add files via upload

---
+++ ActivityA2.py
@@ -0,0 +1,20 @@
+#Assign different variables 
+name="Gatik"
+age=14
+is_student=True
+Weight=40.5
+#printing different variables and theredata type
+print("Name:", name)
+print("Data type of name:",type (name))
+print("Age:", age)
+print("Data type of age:",type (age))
+print("Are you a student:", is_student)
+print("Data typeof student",type (is_student))
+# Type casting to convert the data type of variables
+print("\n After type casting")
+age=str(age)
+print("Age:",age)
+print("Type of age is:",type (age))
+Weight=int(Weight)
+print("Weight:",Weight)
+print("Data type of weight:",type (Weight))
---
+++ ActivityA3.py
@@ -0,0 +1,7 @@
+# input A word
+text=str(input("Enter a word:"))
+# reversing the string
+# using the step value as -1 to iterate in reverse
+reversetext=text[::-1]
+text=reversetext
+print("Reverse of given string is:",text) 
