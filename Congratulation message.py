c05e78d7d884bb3241a0bde9a494a5849c12a3d1
vikaswilson1980-creator <vikaswilson1980@gmail.com>
2025-09-27 12:26:21 +0530

    Add files via upload

---
+++ Congratulation message.py
@@ -0,0 +1,9 @@
+name="Pragati"
+marks="93 percent"
+Grade=8
+print("Congratulation",name )
+print("Data type of name:",type (name))
+print("Marks:",marks)
+print("Data type of marks:",type (marks))
+print("In grade",Grade)
+print("Data type of Grade:",type (Grade))
